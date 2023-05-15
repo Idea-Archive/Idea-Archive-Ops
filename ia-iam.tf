2c8b0a26194b7ea5e465bd3094bd29b860896f4a
yohan <noyohanx@naver.com>
2023-05-15 09:56:07 +0900

    :bug: :: 오타 정정

--- ia-iam.tf
+++ ia-iam.tf
@@ -54,3 +54,49 @@ resource "aws_iam_role_policy_attachment" "ia-pipeline-attachment-2" {
     policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
     role = aws_iam_role.ia-codepipeline-role.id
 }
+
+resource "aws_iam_role" "ia-codebuild-role" {
+    name = "ia-codebuild-role"
+
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement" : [
+        {
+            "Action":"sts:AssumeRole",
+            "Principal": {
+                "Service" : "codebuild.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "ia-cicd-build-policies" {
+    statement{
+        sid = ""
+        actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*", "iam:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "ia-cicd-build-policy" {
+    name = "tf-cicde-build-policy"
+    path = "/"
+    description = "Codebuild policy"
+    policy = data.aws_iam_policy_document.ia-cicd-build-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "ia-cicd-codebuild-attachment1" {
+    policy_arn = aws_iam_policy.ia-cicd-build-policy.arn
+    role       = aws_iam_role.ia-codebuild-role.id
+}
+
+resource "aws_iam_role_policy_attachment" "ia-cicde-codebuild-attachment2" {
+    policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+    role       = aws_iam_role.ia-codebuild-role.id
+}
